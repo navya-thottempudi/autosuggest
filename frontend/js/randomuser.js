d0bf67657b143245bbd37006d466e525ec4597d3
Navya-Thottempudi <navyathottempudi0048@gmail.com>
2026-06-20 10:32:14 +0530

    Added random user feature

---
+++ frontend/js/randomuser.js
@@ -0,0 +1,13 @@
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var user=data.results[0];
+            var fullName=user.name.title+" "+user.name.first+" "+user.name.last;
+            document.getElementById("image").src=user.picture.large;
+            document.getElementById("name").innerText=fullName;
+            document.getElementById("gender").innerText=user.gender;
+        })
+}
