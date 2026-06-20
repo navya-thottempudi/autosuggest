42c11a7d1d22b750ecaf25a716fdc495f8ad9120
Navya-Thottempudi <navyathottempudi0048@gmail.com>
2026-06-20 10:01:27 +0530

    Added toggle feature

--- frontend/js/toggle.js
+++ frontend/js/toggle.js
@@ -1,10 +1,14 @@
 var a=[{"image":"/images/john.png","Name":"John Doe","Gender":"Male"},{"image":"/images/jane.png","Name":"Jane Doe","Gender":"Female"}];
 
-var cur=1;
+var cur=0;
 function toggle(){
     cur=1-cur;
     var user=a[cur];
     document.getElementById("image").src=user.image;
-    document.getElementById("Name").textContent=user.Name;
-    document.getElementById("Gender").textContent=user.Gender;
+    document.getElementById("name").textContent=user.Name;
+    //can use either textContent or inner text
+    document.getElementById("gender").innerText=user.Gender;
 }
+//cur=0
+//cur=cur+1;
+//cur=cur%2;
